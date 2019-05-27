85529af78befdfdee43384892e0633a0a17fb744
Timo <ttimoww@gmail.com>
2019-05-27 20:01:35 +0200

    Added UI and changed overall code flow

--- js/index.js
+++ js/index.js
@@ -1,37 +1,13 @@
-/**
- * Start test for each product object in array
- */
-const startTests = () => {
-    const testProduct = (index) => {
-        products[index].doTest(true, function(){
-            counter += 1;
-            console.log(counter);
-
-            if(counter < products.length){
-                testProduct(counter);
-            }
-        })
-    }
-
-    let counter = 0;
-    testProduct(counter); 
-}
-
-/**
- * Put all products to test in an aray
- * @param {function} fn Callback function to run when this function is completed
- */
-const initProducts = (fn) => {
-    const p1 = new Product('https://live-shop.neatlynamed.com/mini-name-labels.html#redirect');
-    const p2 = new Product('https://live-shop.neatlynamed.com/small-name-labels.html#redirect');
-    products.push(p1);
-    products.push(p2);
-    fn();
-}
+let products = ['https://live-shop.neatlynamed.com/mini-name-labels.html#redirect', 'https://live-shop.neatlynamed.com/small-name-labels.html#redirect'];
 
 $(document).ready(function () {
-    products = [];
-    initProducts(function(){
-        startTests();
-    }); 
+    
+    $('#start').click(function(){
+
+        for (let i = 0; i < products.length; i++) {
+            console.log(products[i]);
+            window.open(products[i]);
+        }
+
+    })
 });
